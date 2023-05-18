8c053415fcf0c7aa75e40ac041aafe19441f174b
shashank <shashank.panchal@technostacks.com>
2023-05-18 10:31:28 +0530

    Initial commit

---
+++ src/init/routes.ts
@@ -0,0 +1,9 @@
+import { Express } from 'express';
+
+import { authRouter, userRouter } from '../mvc/routes';
+import { Routes } from '../mvc/routes/routesStrings';
+
+export default (app: Express) => {
+  app.use('/v1', authRouter);
+  app.use(Routes.user, userRouter);
+};
